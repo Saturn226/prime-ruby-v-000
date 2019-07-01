851aa8160e08693cfa72e439a71703b419169dfd
Ashley Muller <serenehopes@gmail.com>
2019-07-01 04:42:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(number)
-  if number < 2
-    return false
-  end
+  # if number < 2
+  #   return false
+  # end
   
   (2...number).to_a.none? do |num|
     number % num == 0
